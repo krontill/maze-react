c295d3a4502bdac5da94c264e147441c48879387
krontill <till.work@gmail.com>
2018-11-19 19:06:33 +0300

    Add maze for GitHub Pages

---
+++ precache-manifest.c681df32f97265ab215e7d028ac6b55b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d3b6ac5969fa008fdb7",
+    "url": "/static/js/main.6d3b6ac5.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "6d3b6ac5969fa008fdb7",
+    "url": "/static/css/main.1e3a49d2.chunk.css"
+  },
+  {
+    "revision": "a1e2a3cb972ede9c6ad734e8f63ce114",
+    "url": "/index.html"
+  }
+];
